355a4d889ec986e2e2cef0b0e4b59aeeb868d6de
Janek Thomaschewski <janek@jbbr.net>
2017-10-19 01:22:06 +0200

    Hide output of gnome-keyring load

--- gnome-keyring-daemon/gnome-keyring.zsh
+++ gnome-keyring-daemon/gnome-keyring.zsh
@@ -2,7 +2,7 @@
 if (( $+commands[gnome-keyring-daemon] )); then
   envfile="$HOME/.gnome-keyring.env"
   if ! pgrep -f 'gnome-keyring-daemon.*components.*gpg,ssh,secrets' > /dev/null ; then
-    killall gnome-keyring-daemon
+    killall gnome-keyring-daemon &> /dev/null
     gnome-keyring-daemon --start --components=gpg,ssh,secrets >! $envfile
   fi
   eval $(cat "$envfile")
