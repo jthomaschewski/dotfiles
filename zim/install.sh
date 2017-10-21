38afcc0955b4103e63c3914f289e103cfc175743
Janek Thomaschewski <janek@jbbr.net>
2017-10-21 23:50:29 +0200

    Add support for ZSH < 5.2
    Checkout zsh-5.0 branch of ZIM in that case

--- zim/install.sh
+++ zim/install.sh
@@ -7,5 +7,13 @@ then
   echo ">>> Zim found, skip ..."
 else
   echo ">>> Zim not found, clone ..."
-  git clone --recursive https://github.com/Eriner/zim.git $HOME/.zim
+  git clone https://github.com/Eriner/zim.git $HOME/.zim
+
+  cd "$HOME/.zim"
+  if [[ $(zsh --version | awk '{print $2}') < 5.2.0 ]]; then
+    echo ">>> ZSH < 5.2 detected, installing legacy zim"
+    git checkout zsh-5.0
+  fi
+  
+  git submodule update --init --recursive
 fi
