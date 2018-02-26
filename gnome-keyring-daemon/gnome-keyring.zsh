277bf91342fbf466b0351a331d56ac75a8061ea8
Janek Thomaschewski <janek@jbbr.net>
2018-02-27 00:56:08 +0100

    Some minor changes

--- gnome-keyring-daemon/gnome-keyring.zsh
+++ gnome-keyring-daemon/gnome-keyring.zsh
@@ -2,7 +2,7 @@
 # Not required when started within a cinnamon or gnome session
 # enable if you use i3 without https://github.com/jbbr/i3-cinnamon
 #
-# if (( $+commands[gnome-keyring-daemon] )); then
+#if (( $+commands[gnome-keyring-daemon] )); then
 #   envfile="$HOME/.gnome-keyring.env"
 #   if ! pgrep -f 'gnome-keyring-daemon.*components.*gpg,ssh,secrets' > /dev/null ; then
 #     killall gnome-keyring-daemon &> /dev/null
